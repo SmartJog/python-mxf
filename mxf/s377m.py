cf763466ddc2d2bd952f79392e97190eafdb3708
Gilles Dartiguelongue <gilles.dartiguelongue@smartjog.com>
2010-10-11 14:23:57 +0200

    Add SMTPE 377M helper objects

---
+++ mxf/s377m.py
@@ -0,0 +1,300 @@
+# -*- coding: utf-8 -*-
+
+""" Implements basic classes to parse SMPTE S377-1-2009 compliant MXF files. """
+
+import re
+
+from mxf.common import InterchangeObject
+from mxf.rp210 import RP210
+
+class S377MException(Exception):
+    """ Raised on non SMPTE 377M input. """
+
+    def __init__(self, error):
+        """ Init Method """
+        Exception.__init__(self, 'S377M: ' + error)
+
+
+class KLVFill(InterchangeObject):
+    """ KLVFill parser. """
+
+    def __init__(self, fdesc, debug=False):
+        InterchangeObject.__init__(self, fdesc, debug)
+
+    def __str__(self):
+        return "<KLVFill size=%d>" % self.length
+
+    def read(self):
+        """ KLV Fill data has no value. """
+
+        if self.debug:
+            print "data:", self.fdesc.read(self.length).encode('hex_codec')
+        else:
+            self.fdesc.seek(self.fdesc.tell() + self.length)
+
+
+class KLVDarkComponent(KLVFill):
+    """ Generic Dark data handler class. """
+
+    def __init__(self, fdesc, debug=False):
+        KLVFill.__init__(self, fdesc, debug)
+
+    def __str__(self):
+        return "<KLVDarkComponent ul=%s size=%d>" % (self.key.encode('hex_codec'), self.length)
+
+
+class MXFPartition(InterchangeObject):
+    """ MXF Partition Pack parser. """
+
+    part_items = [
+        ('major_version',       2),
+        ('minor_version',       2),
+        ('kag_size',            4),
+        ('this_partition',      8),
+        ('previous_partition',  8),
+        ('footer_partition',    8),
+        ('header_byte_count',   8),
+        ('index_byte_cout',     8),
+        ('index_sid',           4),
+        ('body_offset',         8),
+        ('body_sid',            4),
+        ('operational_pattern', 16),
+    ]
+
+    def __init__(self, fdesc, debug=False):
+        InterchangeObject.__init__(self, fdesc, debug)
+        self.data = {'essence_containers': [], }
+
+        if not re.search('060e2b34020501010d01020101(0[2-4])(0[0-4])00', self.key.encode('hex_codec')):
+            raise S377MException('Not a valid Partition Pack key: %s' % self.key.encode('hex_codec'))
+
+    def __str__(self):
+        return '<MXF%(type)sPartition %(openness)s and %(completeness)s>' % {
+            'type': {'\x02': 'Header', '\x03': 'Body', '\x04': 'Footer'}[self.key[13]],
+            'openness': ord(self.key[14]) & 0xfe and 'Closed' or 'Open',
+            'completeness': ord(self.key[14]) & 0xfd and 'Complete' or 'Incomplete',
+        }
+
+    def __smtpe_377m_check(self):
+        """ Check conformance to SMTPE 377M 2004. """
+
+        if self.data['major_version'].encode('hex_codec') != '0001':
+            raise S377MException('Invalid Major version for Partition Pack')
+        if self.data['minor_version'].encode('hex_codec') not in ('0002', '0003'):
+            raise S377MException('Invalid Minor version for Partition Pack')
+
+        # Header Partition Pack checks
+        if self.key[14] == '\x02':
+            if self.data['this_partition'] != 8 * '\x00':
+                raise S377MException('Invalid value for ThisPartition in Header Partition Pack')
+            if self.data['previous_partition'] != 8 * '\x00':
+                raise S377MException('Invalid value for PreviousPartition in Header Partition Pack')
+        # partition_info['operational_pattern'][13] -> 10h –7Fh specialized pattern
+
+        # Footer Patition Pack checks
+        if self.key[14] == '\x04':
+            if not ord(self.key[14]) & 0xfe:
+                raise S377MException('Open Footer Partition is not allowed')
+
+        if len(self.data['essence_containers']) == 0 and self.data['body_sid'] != 4 * '\x00':
+            raise S377MException('Invalid value for BodySID in Partition Pack')
+
+    def read(self):
+        idx = 0
+        data = self.fdesc.read(self.length)
+
+        # Read Partition Pack items
+        for pp_item, pp_item_size in self.part_items:
+            self.data[pp_item] = data[idx:idx+pp_item_size]
+            idx += pp_item_size
+
+        # Read essence containers list, if any
+        ec_list_size = self.ber_decode_length(data[idx:idx+4], 4)
+        ec_item_size = self.ber_decode_length(data[idx+4:idx+8], 4)
+
+        idx = 8
+        while ec_list_size > len(self.data['essence_containers']):
+            self.data['essence_containers'].append(data[idx:idx+ec_item_size])
+            idx += ec_item_size
+
+        self.__smtpe_377m_check()
+
+        if self.debug:
+            print "%d essence containers of %d size in partition:" % (ec_list_size, ec_item_size)
+
+        return
+
+    def human_readable(self):
+        for key, item in self.data.items():
+            if key == 'essence_containers':
+                for i, essence in enumerate(item):
+                    print "Essence %d: " % i, essence.encode('hex_codec')
+            else:
+                print "%s: %s" % (key, item.encode('hex_codec'))
+        return
+
+
+class MXFPrimer(InterchangeObject):
+    """ MXF Primer Pack parser. """
+
+    def __init__(self, fdesc, debug=False):
+        InterchangeObject.__init__(self, fdesc, debug)
+        self.data = {}
+        self.rp210_convert = RP210()
+
+        if self.key and not re.search('060e2b34020501..0d01020101050100', self.key.encode('hex_codec')):
+            raise S377MException('Not a valid Primer Pack key: %s' % self.key.encode('hex_codec'))
+
+
+    def __str__(self):
+        ret = ['<MXFPrimer']
+        ret += ['size=%d' % self.length]
+        ret += ['localtags=%d' % len(self.data)]
+        if self.debug:
+            ret += ['\n']
+            data_keys = self.data.keys()
+            data_keys.sort()
+            for i in data_keys:
+                ret += ['%s: %s\n' % (i.encode('hex_codec'), self.data[i].encode('hex_codec'))]
+        return ' '.join(ret) + '>'
+
+    def read(self):
+
+        data = self.fdesc.read(self.length)
+
+        lt_list_size = self.ber_decode_length(data[0:4], 4)
+        lt_item_size = self.ber_decode_length(data[4:8], 4)
+
+        idx = 8
+        while lt_list_size > len(self.data):
+            self.data[data[idx:idx+2]] = data[idx+2:idx+lt_item_size]
+            idx += lt_item_size
+
+        if self.debug:
+            print "%d local tag mappings of %d size in Primer Pack" % (lt_list_size, lt_item_size)
+
+        return
+
+    def convert(self, tag, value):
+        etag = tag.encode('hex_codec')
+        evalue = value.encode('hex_codec')
+
+        if tag not in self.data.keys():
+            return "Error: Local key '%s' not found in primer (%s)" % (etag, evalue)
+
+        if not self.data[tag].startswith('060e2b34'.decode('hex_codec')):
+            return "Error: '%s' does not map to a SMPTE format UL '%s'" % (etag, self.data[tag].encode('hex_codec'))
+
+        # SMTPE RP 210 conversion
+        return self.rp210_convert.convert(self.data[tag], value)
+
+
+class MXFDataSet(InterchangeObject):
+    """ MXF parsing class specialized for loading Sets and Packs. """
+
+    def __init__(self, fdesc, primer, debug=False, dark=False):
+        InterchangeObject.__init__(self, fdesc, debug)
+        self.primer = primer
+        self.dark = dark
+        self.data = {}
+
+        if not self.dark:
+            if not self.key.encode('hex_codec').startswith('060e2b34'):
+                raise S377MException('Not a SMPTE administrated label')
+
+            if self.key[4] != '\x02':
+                raise S377MException('Not an MXF Set/Pack')
+
+            if self.key[5] != '\x53':
+                raise S377MException('Non-Local set syntax not supported yet (0x%x)' % ord(self.key[5]))
+
+    def __str__(self):
+        ret = ['<MXF' + (self.dark and 'Dark' or '') + 'DataSet']
+        ret += ['size=%d' % self.length]
+        ret += ['InstanceUID=%s' % self.data['\x3c\x0a'].encode('hex_codec')]
+        if self.debug:
+            ret += ['tags=%d:\n' % len(self.data) \
+                + '\n'.join(["%s: %s %d bytes" % (
+                    i.encode('hex_codec'),
+                    j.encode('hex_codec').ljust(64, ' ')[:64],
+                    len(j)
+                ) for i, j in self.data.items()])]
+        return ' '.join(ret) + '>'
+
+    def read(self):
+        """ Generic read method for sets and packs. """
+
+        idx = 0
+        data = self.fdesc.read(self.length)
+
+        # Get all items
+        offset = idx
+        while offset < idx + self.length:
+            set_size = self.ber_decode_length(data[offset+2:offset+4], 2)
+            self.data.update({data[offset:offset+2]: data[offset+4:offset+set_size+4]})
+            offset += set_size + 4
+
+        return
+
+    def human_readable(self, klv_hash=None, indent=None):
+
+        if not indent:
+            indent = 0
+
+        print "%s%s" % (4 * indent * ' ', self)
+
+        for i, j in self.data.items():
+            print "%s%s" % (4 * indent * ' ' + '  ', self.primer.convert(i, j))
+
+        return klv_hash
+
+
+class MXFPreface(MXFDataSet):
+    """ MXF Metadata Preface parser. """
+
+    def __init__(self, fdesc, debug=False):
+        MXFDataSet.__init__(self, fdesc, debug)
+
+    def __str__(self):
+        """ Render function. """
+        ret = ['<MXFPreface']
+        ret += ['size=%d' % self.length]
+        ret += ['InstanceUID=%s' % self.data['\x3c\x0a'].encode('hex_codec')]
+        if self.debug:
+            ret += ['tags=%d:\n' % len(self.data) \
+                + '\n'.join(["%s: %s %d bytes" % (
+                    i.encode('hex_codec'),
+                    j.encode('hex_codec').ljust(64, ' '),
+                    len(j)
+                ) for i, j in self.data.items()])]
+        return ' '.join(ret) + '>'
+
+
+class RandomIndexMetadata(InterchangeObject):
+    """ MXF Random Index Pack metadata parser. """
+
+    def __init__(self, fdesc, debug=False):
+        InterchangeObject.__init__(self, fdesc, debug)
+        self.data = {'partition': []}
+
+    def __str__ (self):
+        return '<RandomIndexMetadata size=%d entries=%d>' % (self.length, len(self.data['partition']))
+
+    def read(self):
+
+        idx = 0
+        data = self.fdesc.read(self.length)
+
+        for _ in range(0, (self.length - 4) / 12):
+            self.data['partition'].append({
+                'body_sid': data[idx:idx+4],
+                'byte_offset': data[idx+4:idx+12],
+            })
+            idx += 12
+
+        total_part_length = self.ber_decode_length(data[idx:idx+4], 4)
+
+        if 16 + self.bytes_num + self.length != total_part_length:
+            raise S377MException('Overall length differs from UL length')
+        return
+
