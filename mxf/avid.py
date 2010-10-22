bafaf7592b35a328e4dd93513fd86a13b99e04fb
Gilles Dartiguelongue <gilles.dartiguelongue@smartjog.com>
2010-10-22 18:11:13 +0200

    avid: use MXFPrimer customize method for dark metadata handling

--- mxf/avid.py
+++ mxf/avid.py
@@ -2,8 +2,9 @@
 
 """ Implements basic classes to parse Avid specific MXF objects. """
 
-from mxf.common import InterchangeObject, OrderedDict
-from mxf.s377m import MXFDataSet
+from mxf.common import InterchangeObject
+from mxf.s377m import MXFDataSet, MXFPrimer
+from mxf.rp210 import RP210Avid
 from mxf.rp210types import Reference, Integer
 
 class AvidObjectDirectory(InterchangeObject):
@@ -64,77 +65,15 @@ class AvidObjectDirectory(InterchangeObject):
 class AvidMetadataPreface(MXFDataSet):
     """ Avid metadata dictionary pseudo Preface parser. """
 
+    _extra_mappings = {
+        '0001': ('StrongReference', 'AAF Metadata', 'Avid AAF Metadata Reference'),
+        '0002': ('StrongReference', 'Preface', 'Avid Preface Reference'),
+        '0003': ('AvidOffset', 'Object Directory', 'Position of the Object Directory'),
+        '0004': ('UInt32', 'Audio Channels', 'Number of audio channels in source file'),
+    }
+
     def __init__(self, fdesc, primer, debug=False):
-        MXFDataSet.__init__(self, fdesc, primer, debug, dark=True)
-        self.data = {
-            'by_tag': OrderedDict(),
-            'by_format_ul': OrderedDict(),
-        }
-
-    def __str__(self):
-        ret = ['<AvidMetadataPreface']
-        ret += ['pos=%d' % self.pos]
-        ret += ['size=%d' % self.length]
-        ret += ['InstanceUID=%s' % self.data['by_tag']['\x3c\x0a'].encode('hex_codec')]
-        return ' '.join(ret) + '>'
-
-    def read(self):
-        """ Generic read method for sets and packs. """
-
-        idx = 0
-        data = self.fdesc.read(self.length)
-
-        # Get all items
-        offset = idx
-        while offset < idx + self.length:
-            set_size = self.ber_decode_length(data[offset+2:offset+4], 2)
-            localtag = data[offset:offset+2]
-            localdata = data[offset+4:offset+set_size+4]
-            self.data['by_tag'].update({localtag: localdata})
-
-            cvalue = None
-            key_name = None
-            if localtag.encode('hex_codec') == '0003':
-                cvalue = 'Duration in a very long form ?'
-                key_name = 'duration'
-            elif localtag.encode('hex_codec') == '0004':
-                cvalue = 'Number of audio channels in an Int64 ?'
-                key_name = 'audio_channels'
-            else:
-                try:
-                    key_name, cvalue = self.primer.decode_from_local_tag(localtag, localdata)
-
-                except Exception, _error:
-                    print "Could not convert to [data:%s] format %s" % (localdata.encode('hex_codec'), self.primer.data[localtag].encode('hex_codec'))
-                    raise
-
-            self.data['by_format_ul'].update({key_name: cvalue})
-
-            offset += set_size + 4
-
-        return
-
-    def human_readable(self, klv_hash=None, indent=None):
-
-        if not indent:
-            indent = 0
-
-        print "%s%s" % (4 * indent * ' ', self)
-
-        for i, j in self.data['by_format_ul'].items():
-            if i in ('preface', 'aaf_metadata'):
-                if j.read() not in klv_hash:
-                    print "%s%s: broken reference, %s" % (4 * indent * ' ' + '  ', i, j)
-                else:
-                    print ""
-                    klv_hash.pop(j.read()).human_readable(klv_hash, indent+1)
-
-            elif i == 'guid':
-                continue
-
-            else:
-                print "%s%s: %s %s" % (4 * indent * ' ' + '  ', i, j, type(j))
-
-        return klv_hash
-
+        aprim = MXFPrimer.customize(primer, RP210Avid(), self._extra_mappings)
+        MXFDataSet.__init__(self, fdesc, aprim, debug=debug, dark=True)
+        self.set_type = 'AvidMetadataPreface'
 
